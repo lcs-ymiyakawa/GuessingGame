efcd590e6782f21de835f74eaa788afd924b0c59
Yoshi Miyakawa <ymiyakawa@lcs.on.ca>
2024-01-30 11:36:43 -0500

    Re-organized project and created a GameView file.

---
+++ GuessingGame/GameView.swift
@@ -0,0 +1,18 @@
+//
+//  GameView.swift
+//  GuessingGame
+//
+//  Created by 宮川義之助 on 2024/01/30.
+//
+
+import SwiftUI
+
+struct GameView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GameView()
+}
