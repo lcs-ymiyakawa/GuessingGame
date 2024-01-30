b2fbb2ec770aebaf744f85b9a44e393aa8f49c31
Yoshi Miyakawa <ymiyakawa@lcs.on.ca>
2024-01-30 11:33:13 -0500

    Initial Commit

---
+++ GuessingGame/GuessingGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessingGameApp.swift
+//  GuessingGame
+//
+//  Created by 宮川義之助 on 2024/01/30.
+//
+
+import SwiftUI
+
+@main
+struct GuessingGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
